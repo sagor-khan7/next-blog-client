0f0aa938769e77fe677b6eb34ff0c091fffbfcd8
sagor khan <sksagor791@gmail.com>
2026-08-25 19:36:56 +0600

    set up navbar, darkMode, added dashboard from shadcn blocks

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+export default function AboutPage() {
+  //* Simulating server response
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
+
+  //* Simulating server error
+  // throw new Error("Something went wrong...");
+
+  return (
+    <div>
+      <h1>This is about page</h1>
+    </div>
+  );
+}
---
+++ src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BranchPage() {
+  return (
+    <div>
+      <h1>This is branch page from contact</h1>
+    </div>
+  );
+}
