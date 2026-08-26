0ae6a322572e1b2de8f830a2b8dbc40a0c8bc6bb
sagor khan <sksagor791@gmail.com>
2026-08-26 23:57:41 +0600

    Get Session data in server component using fetch

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { cookies } from "next/headers";
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+  console.log(cookieStore.toString());
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+  const session = await res.json();
+  console.log(session);
   return (
     <div>
       <Button variant="outline">Click here</Button>
