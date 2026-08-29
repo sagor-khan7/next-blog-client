e6b7847b80bd6a4f9486753f3b85c8538c44b26a
sagor khan <sksagor791@gmail.com>
2026-08-29 09:23:23 +0600

    Get post by id and Understanding the purpose of generateStaticParams

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -56,4 +56,16 @@ export const blogService = {
       return { data: null, error: { message: "Something went wrong!" } };
     }
   },
+
+  getBlogById: async function (id: string) {
+    try {
+      const res = await fetch(`${API_URL}/posts/${id}`);
+
+      const data = await res.json();
+
+      return { data: data, error: null };
+    } catch (error) {
+      return { data: null, error: { message: "Something went wrong!" } };
+    }
+  },
 };
