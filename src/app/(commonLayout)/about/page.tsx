15a567b90222360f8f8ea7aa6597b5bc7ff3c4ee
sagor khan <sksagor791@gmail.com>
2026-08-29 11:18:04 +0600

    Understanding server function and use server

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { getBlogs } from "@/actions/blog.action";
+import { useEffect, useState } from "react";
+
 export default function AboutPage() {
   //* Simulating server response
   // await new Promise((resolve) => setTimeout(resolve, 4000));
@@ -5,6 +10,18 @@ export default function AboutPage() {
   //* Simulating server error
   // throw new Error("Something went wrong...");
 
+  const [data, setData] = useState();
+  const [error, setError] = useState<{ message: string } | null>(null);
+  console.log(data);
+  console.log(error);
+
+  useEffect(() => {
+    (async () => {
+      const { data, error } = await getBlogs();
+      setData(data);
+      setError(error);
+    })();
+  }, []);
   return (
     <div>
       <h1>This is about page</h1>
